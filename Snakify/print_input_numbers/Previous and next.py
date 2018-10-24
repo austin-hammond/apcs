57cdcc304a0ce184d18bb558fcdb95b68e23c85f
Austin Hammond <austin.hammond@student.rjuhsd.us>
2018-10-24 20:40:11 +0000

    Snakify Final

---
+++ Snakify/print_input_numbers/Previous and next.py
@@ -0,0 +1,8 @@
+# Read an integer:
+a = int(input())
+# Read a float:
+b = str(a + 1)
+c = str(a - 1)
+# Print a value:
+print("The next number for the number", a, "is", b + ".")
+print("The previous number for the number", a, "is", c + ".")
